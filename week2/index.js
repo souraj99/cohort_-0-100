4b1ff4bf581b6d40debde1d2fe7134db754185ee
souraj99 <sourajghosh9@gmail.com>
2024-08-21 11:03:44 +0530

    express setup

--- week2/index.js
+++ week2/index.js
@@ -1,8 +1,25 @@
 // creating a new http server using express
 
-import express from 'express';
+ const express = require('express') ;
 
 const app = express();
+const port = 3000;
+
+const sum =(num)=>{
+let ans = 0;
+for (let i = 0; i<num; i++) ans += i;
+return ans;
+}
+
+app.get('/', (req, res) => {
+    const query = parseInt(req.query.num);
+    if (isNaN(query) || query < 0) {
+      return res.sendStatus(400); 
+    }
+    const result = sum(query);
+    res.send(result.toString()); 
+
+});
 
 
-
+app.listen(port);
